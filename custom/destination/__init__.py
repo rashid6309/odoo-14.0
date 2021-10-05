cf32cd6d8d246221ebe3f14f725f93a159d6e508
rashid <@rashid6309>
2021-10-05 15:32:36 +0500

    new changes

--- custom/destination/__init__.py
+++ custom/destination/__init__.py
@@ -1,1 +1,1 @@
-from .models import record
+from .models import destination
--- custom/destination/models/destination.py
+++ custom/destination/models/destination.py
@@ -1,20 +1,13 @@
 from odoo import models, fields
 
 
-class CustomerRecord(models.Model):
-    _name = "customer.record"
-    _description = "Customer Record"
+class Destination(models.Model):
+    _name = "destination.package"
+    _description = "Destination Package Details"
 
-    customer_name = fields.Char(string="Name", require=True)
-    customer_age = fields.Integer("Age")
-    customer_address = fields.Char(string="Address", require=True)
-    customer_contact = fields.Char(string="Contact", require=True)
-    customer_email = fields.Char(string="Email", require=True)
-    customer_picture = fields.Binary(string="Picture")
+    package_name = fields.Char(string="Name", require=True)
+    package_days = fields.Integer("days")
+    city = fields.Char(string="City", require=True)
+    package_details = fields.Char(string="Details", require=True)
+    contact_details = fields.Char(string="Contact", require=True)
 
-
-class CustomerRecordRelation(models.Model):
-    _name = "customer.record.data"
-    _description = "Customer Record Data"
-    report_id = fields.Many2one("account.report", string="Report")
-    recordId=fields.Many2one("customer.record",string="recordID")
