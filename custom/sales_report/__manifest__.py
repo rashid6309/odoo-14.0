b1c80847b9945572d082f0e762a2dc585054de13
rashid <@rashid6309>
2021-10-05 14:51:34 +0500

    new changes

---
+++ custom/sales_report/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Customer Data',
+    'version': '1.1',
+    'author': 'Rashid-Odoo-Master',
+    'summary': 'Customer System Data',
+    'sequence': 10,
+    'description': """ this is summary description""",
+    'category': 'Extra Tools',
+    'website': 'https://www.odoo.com/page/billing',
+    'images': ['images/accounts.jpeg', 'images/bank_statement.jpeg', 'images/cash_register.jpeg',
+               'images/chart_of_accounts.jpeg', 'images/customer_invoice.jpeg', 'images/journal_entries.jpeg'],
+    'depends': ['base_setup', 'product', 'analytic', 'portal', 'digest'],
+    'data': [
+        'templates/customer.xml', 'templates/report.xml', 'security/ir.model.access.csv'
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
+
---
+++ custom/sales_report/models/report.py
@@ -0,0 +1,12 @@
+from odoo import models, fields
+
+
+class SalesReport(models.Model):
+    _name = "sales.report"
+    _description = "Customer Sales Report"
+
+    shipment_address = fields.Char(string="Address", require=True)
+    total_amount = fields.Integer("Total")
+    total_commission = fields.Char(string="Commission", require=True)
+    sales_code = fields.Char(string="Code", require=True)
+    sales_date = fields.Date(string="Date")
